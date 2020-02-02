e546ecb36be5c066a122326212a0f8ea11fcc855
ss-koishi <yuuki100201@gmail.com>
2020-02-03 00:31:30 +0900

    add class-4

--- detection.py
+++ detection.py
@@ -8,86 +8,122 @@ import numpy as np
 import cv2
 import sys
 import time
+import threading
 
-if __name__ == '__main__':
+from pymodbus.client.sync import ModbusTcpClient as mtc
+
+import platform
+from smb.SMBConnection import SMBConnection as samba
+
+labels = []
+
+def start():
+    send_prediction(prediction(get_image()))
+
+
+def send_prediction(pred):
+    MODBUS_IP = '192.168.1.1' # modbus ip address
+    destination = '12345' # register address
+
+    client = mtc(MODBUS_IP)
+    client.connect()
+
+    data = 0
+    if pred == 'OK':
+        data = 0
+    else if pred == 'NG':
+        data = 1
+    else:
+        data = 3
+
+    client.write_register(destination, data)
+
+
+def get_image():
+    user = 'hoge'
+    password = 'hoge'
+    server = 'hoge'
+    server_ip = '192.168.1.1'
+    home = 'temp'
+    path = 'hoge/hoge'
+
+    pipe = samba(user, password, platform.node(), server)
+    pipe.connect(server_ip, 139)
+
+    ret = None # stored Sharedfile class
+    items = pipe.listPath(home, path)
+    for item in items:
+        # any process
+
+    # download image
+    local_path = './cached/images'
+    with open(local_path, 'wb') as f:
+        pipe.retrieveFile(ret, os.path.join(home, path), f)
+
+
+    return cv2.imread(os.path.join(local_path, ret.filename))
+
+
+def prediction(img):
+    X = []
+    img = cv2.resize(img, (150, 150))
+
+    # convert grayscale
+    # gamma = np.array([pow(x/255.0, 2.2) for x in range(256)], dtype='float32')
+    # img = cv2.LUT(img, gamma)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = pow(img, 1.0/2.2) * 255
+
+    img = img_to_array(img)
+    img = img / 255
+    X.append(img)
+    X = np.asarray(X)
+    start = time.time()
+    preds = model_pred.predict(X)
+    elapsed_time = time.time() - start
+
+    pred_label = ""
+
+    label_num = 0
+    tmp_max_pred = 0
+    for i in preds[0]:
+        if i > tmp_max_pred:
+            pred_label = labels[label_num]
+            tmp_max_pred = i
+        label_num += 1
+
+    return pred_label
+
+
+def sheduler(interval, target, wait = True):
+    base_time = time.time()
+    next_time = 0
+    while True:
+        t = threading.Thread(target = target)
+        t.start()
+        if wait:
+            t.join()
+        next_time = ((base_time - time.time()) % interval) or intertval
+        time.sleep(next_time)
+
+def main():
     # parse options
-    parser = argparse.ArgumentParser(description='keras-pi.')
+    parser = argparse.ArgumentParser(description='detection')
     parser.add_argument('-m', '--model', default='./model/mnist_deep_model.json')
-    parser.add_argument('-w', '--weights', default='./model/weights.99.hdf5')
+    parser.add_argument('-w', '--weights', default='./model/model.h5')
     parser.add_argument('-l', '--labels', default='./model/labels.txt')
-    parser.add_argument('-d', '--device', default='normal_cam') # normal_cam /jetson_nano_raspi_cam
-
     args = parser.parse_args()
 
-    labels = []
     with open(args.labels,'r') as f:
         for line in f:
             labels.append(line.rstrip())
     print(labels)
 
-    model_pred = load_model(args.model);
+    model_pred = load_model(args.weights)
 
-    # model_pred.summary()
-    if args.device == 'normal_cam':
-        cam = cv2.VideoCapture(0)
-    elif args.device == 'jetson_nano_raspi_cam':
-        GST_STR = 'nvarguscamerasrc \
-            ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)21/1 \
-            ! nvvidconv ! video/x-raw, width=(int)640, height=(int)480, format=(string)BGRx \
-            ! videoconvert \
-            ! appsink'
-        cam = cv2.VideoCapture(GST_STR, cv2.CAP_GSTREAMER) # Raspi cam
-    else:
-        print('wrong device')
-        sys.exit()
+    scheduler(60, start)
 
-    count_max = 0
-    count = 0
 
-    while True:
-        ret, capture = cam.read()
-        if not ret:
-            print('error')
-            break
-        key = cv2.waitKey(1)
-        if key == 27: # when ESC key is pressed break
-            break
 
-        count += 1
-        if count > count_max:
-            X = []
-            img = capture.copy()
-            img = cv2.resize(img, (64, 64))
-            img = img_to_array(img)
-            img = img/255
-            X.append(img)
-            X = np.asarray(X)
-            start = time.time()
-            preds = model_pred.predict(X)
-            elapsed_time = time.time() - start
-
-            pred_label = ""
-
-            label_num = 0
-            tmp_max_pred = 0
-            for i in preds[0]:
-                if i > tmp_max_pred:
-                    pred_label = labels[label_num]
-                    tmp_max_pred = i
-                label_num += 1
-
-            # Put speed
-            speed_info = '%s: %f' % ('speed=', elapsed_time)
-            # print(speed_info)
-            cv2.putText(capture, speed_info , (10,50), \
-              cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-
-            # Put label
-            cv2.putText(capture, pred_label, (10,100), \
-              cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-
-            cv2.imshow('keras-pi inspector', capture)
-            count = 0
-
-    cam.release()
-    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
